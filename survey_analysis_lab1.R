a1f4fcae36ce2ec8adc8bbcdc8d3cfc6015d762a
honggao110 <honggao@nyu.edu>
2015-12-03 13:27:56 -0500

    Split survey_analysis.R into two labs.

---
+++ survey_analysis_lab1.R
@@ -0,0 +1,192 @@
+#install.packages('psych')
+#install.packages('plyr')
+#install.packages('gtools')
+#install.packages('foreach')
+#install.packages('doParallel')
+#install.packages('ggplot2')
+#install.packages('Hmisc')
+#install.packages('reshape2')
+
+library(psych)
+library(plyr)
+library(gtools)
+library(foreach)
+library(doParallel)
+library(ggplot2)
+library(reshape2)
+# library(Hmisc)
+
+###################
+# Configuration   #
+###################
+
+csv_filename = "data/turk_job_reproductive_autonomy_wave_78_assignments.csv"
+data_columns = c("Id", "abortion", "baby", "whether_use", "gender")
+
+###################
+# data processing #
+###################
+
+# set file directory
+setwd("~/")
+
+
+# import csv file
+data<- read.csv(csv_filename, stringsAsFactors = F)
+
+# only keep submitted surveys
+data_raw<- subset(data, AssignmentStatusCode=="Submitted"| AssignmentStatusCode=="Approved", 
+                  select=data_columns)
+
+# rename item names
+#for (i in 1:(ncol(data_raw)-1)){
+#  names(data_raw)[i+1] <- paste0("item",i)
+#}
+# reverse code items
+#reverse_items<- c(paste0("item",c(3:5, 9:12)))
+#data_raw[,reverse_items]<- 6 - data_raw[,reverse_items]
+
+data_raw$total<- rowSums(data_raw[,2:ncol(data_raw)])
+data_raw$mean<- rowMeans(data_raw[,2:(ncol(data_raw)-1)])
+
+write.table(data_raw, file="data_raw.csv", sep=",", row.names = F)
+
+###############
+# descriptive #
+###############
+
+describe(data_raw)
+
+#############
+# histogram #
+#############
+
+hist(data_raw$total, xlab="total", main="Total", breaks=max(data_raw$total))
+# lines(density(data_raw$total))
+hist(data_raw$mean, xlab="mean", main="Mean", breaks=as.integer(max(data_raw$mean)))
+
+item_name<- names(data_raw[2:(ncol(data_raw)-2)])
+par(mfrow=c(3,2)) # c("#rows,#cols")
+for (i in item_name){
+  hist(data_raw[[i]], xlab=i, main=paste("Histogram of",i))
+}
+
+#################
+# data analysis #
+#################
+
+# variance
+var(data_raw[2:(ncol(data_raw)-2)])
+
+# covariance matrix
+cov(data_raw[2:(ncol(data_raw)-2)], method="pearson")
+cov(data_raw[2:(ncol(data_raw)-2)], method="kendall")
+cov(data_raw[2:(ncol(data_raw)-2)], method="spearman")
+
+# correlation matrix
+cor(data_raw[2:(ncol(data_raw)-2)], method="pearson")
+cor(data_raw[2:(ncol(data_raw)-2)], method="kendall")
+cor(data_raw[2:(ncol(data_raw)-2)], method="spearman")
+
+
+# alpha and interitem correlation
+alpha(data_raw[2:(ncol(data_raw)-2)])
+
+alpha_raw50<- alpha(data_raw[2:(ncol(data_raw)-2)])$total$raw_alpha
+alpha_std50<- alpha(data_raw[2:(ncol(data_raw)-2)])$total$std.alpha
+average_r50<- alpha(data_raw[2:(ncol(data_raw)-2)])$total$average_r
+
+
+
+###### resampling ######
+
+# draw 5, 10, 15, 20, 25 samples from the serveys
+# repeat for 1000 times and extract mean alpha and interitem correlation
+draw_sample<- c(10, 15, 20, 25, 30, 40) # the number of sample being drawn
+# define a resample function to keep code clean
+resample.function<- function(data_raw, draw_sample, x){
+  n<- nrow(data_raw) # count total number of observations
+  N<- x # the number of times to resample
+  alpha_v<- numeric(N)# create a vector to store the results
+  alpha_stdv<- numeric(N)
+  average_r<- numeric(N)
+  resample<- foreach(i=draw_sample, .combine='rbind') %dopar%{
+    for (j in 1:N){
+      obs<- sample(n, i, replace=F)
+      samp<- data_raw[obs,]
+      alpha_list<- alpha(samp[2:(ncol(samp)-2)])
+      alpha_v[j]<- alpha_list$total$raw_alpha # extract raw alpha value
+      alpha_stdv[j]<- alpha_list$total$std.alpha # extract standardized alpha value
+      average_r[j]<- alpha_list$total$average_r # extract average r value
+    }
+    alpha_mean<- mean(alpha_v)
+    alpha_lower<- alpha_mean - qnorm(0.975)*sd(alpha_v) # calculate confidence interval
+    alpha_higher<- alpha_mean + qnorm(0.975)*sd(alpha_v)
+    alpha_stdmean<- mean(alpha_stdv)
+    alpha_stdlower<- alpha_stdmean - qnorm(0.975)*sd(alpha_stdv)
+    alpha_stdhigher<- alpha_stdmean + qnorm(0.975)*sd(alpha_stdv)
+    average_rmean<- mean(average_r)
+    average_rlower<- average_rmean - qnorm(0.975)*sd(average_r)
+    average_rhigher<- average_rmean + qnorm(0.975)*sd(average_r)
+    alphar<- cbind(i, alpha_mean, alpha_lower, alpha_higher, alpha_stdmean, alpha_stdlower, alpha_stdhigher, 
+                   average_rmean, average_rlower, average_rhigher)
+    
+  }
+  return(resample) # specify which data frame to return in a function
+}
+resample<- resample.function(data_raw, draw_sample, 100) # run 100 bootstrap
+# combind with the whole sample
+i<- 50
+alpha_mean<- alpha_raw50
+alpha_stdmean<- alpha_std50
+average_rmean<- average_r50
+alphar50<- cbind(i, alpha_mean, alpha_stdmean, average_rmean)
+resample<- smartbind(resample, alphar50)
+for (i in 1:ncol(resample)){ # replace missing values to the previous value in the same row
+  resample[7,i]<- ifelse(is.na(resample[7,i]), resample[7,i-1], resample[7,i])
+}
+
+
+###### take out item ######
+
+draw_item<- NULL # create an empty object
+for (k in names(data_raw[2:(length(data_raw)-2)])){
+  data_item<- data_raw[ , !names(data_raw) %in% c(i)] # delete one variable at a time
+  data_item_resample<- resample.function(data_item, draw_sample, 10) # 10 bootstrap
+  data_item_resample<- cbind(k, data_item_resample)
+  draw_item<- rbind(draw_item, data_item_resample)
+  i<- 50
+  alpha_mean<- alpha(data_item[2:(ncol(data_raw)-2)])$total$raw_alpha
+  alpha_stdmean<- alpha(data_raw[2:(ncol(data_raw)-2)])$total$std.alpha
+  average_rmean<- alpha(data_raw[2:(ncol(data_raw)-2)])$total$average_r
+  alphar50<- cbind(k, i, alpha_mean, alpha_stdmean, average_rmean)
+  draw_item<- smartbind(draw_item, alphar50)
+}
+for (i in 1:ncol(draw_item)){ # replace missing values
+  draw_item[,i]<- ifelse(is.na(draw_item[,i]), draw_item[,i-1], draw_item[,i])
+}
+
+# export data frame as csv files
+write.table(resample, file="resample.csv", sep=",", row.names=F)
+write.table(draw_item, file="draw_item.csv", sep=",", row.names=F)
+
+############
+# graphics #
+############
+
+resample<- data.frame(resample)
+par(mfrow=c(1,1))
+ggplot(resample, aes(i)) + 
+  geom_line(aes(y = alpha_stdmean), size=1.2, col="black") + 
+  geom_point(aes(y = alpha_stdmean), size=5, col="black")+
+  geom_line(aes(y = alpha_stdlower), size=1.2, col="black", linetype="dashed")+
+  geom_line(aes(y = alpha_stdhigher), size=1.2, col="black", linetype="dashed")+
+  geom_line(aes(y = average_rmean), size=1.2, col="steelblue2")+
+  geom_point(aes(y = average_rmean), size=5, shape=17, col="steelblue2")+
+  geom_line(aes(y = average_rlower), size=1.2, col="steelblue2", linetype="dashed")+
+  geom_line(aes(y = average_rhigher), size=1.2, col="steelblue2", linetype="dashed")+
+  coord_cartesian(ylim=c(0,1))+
+  theme(panel.background=element_blank())+
+  theme(panel.background= element_rect(color="black"))+
+  labs(x="Resample Size", y="Value")
+
---
+++ survey_analysis_lab2.R
@@ -0,0 +1,212 @@
+
+# To run this script, make sure you have data_raw from lab 1 in the upper right window under Data
+
+library(plyr)
+library(ggplot2)
+library(reshape2)
+
+############
+# validity #
+############
+
+## create a dichotomous variable for each question ##
+all_items<- data_columns[-1]
+total_count<- paste0(all_items,"_bin")
+for (i in all_items){
+  data_raw[[paste0(i,"_bin",sep="")]]<- ifelse(data_raw[[i]]<=2, 0, 1)
+}
+N<- length(total_count)
+
+# If you don't have subscales, don't run line 21-29
+# define subscales
+# items raw scored
+raw_items<- c("abortion","baby")
+s1_count<- paste0(raw_items,"_bin")
+a<- length(s1_count)
+# items reverse scored
+reverse_items<- c("whether_use","gender")
+s2_count<- paste0(reverse_items,"_bin")
+b<- length(s2_count)
+
+## basic descriptives ##
+describe(data_raw[,grep("_bin", names(data_raw))])
+
+## Aggregate scores ##
+data_raw$Total_count<- 0
+for (i in c(total_count)){
+  data_raw$Total_count<- data_raw$Total_count+ data_raw[[i]]
+}
+# If you don't have subscales, don't run line 40-47
+data_raw$s1_count<- 0
+for (i in s1_count){
+  data_raw$s1_count<- data_raw$s1_count+ data_raw[[i]]
+}
+data_raw$s2_count<- 0
+for (i in s2_count){
+  data_raw$s2_count<- data_raw$s2_count+ data_raw[[i]]
+}
+
+## Generate TP and TN for each item over set of cutpoints ##
+cutpoint<- 1:N
+for (i in 1:N){
+  for (j in 1:5){
+    data_raw[[paste("q_TP",j,i,sep="_")]]<- ifelse(data_raw$Total_count>=j & data_raw[[paste0("q",i,"_bin")]]==1, 1, 0)
+  }
+}
+for (i in 1:N){
+  for (j in 7:10){
+    data_raw[[paste("q_TN",j,i,sep="_")]]<- ifelse(data_raw$Total_count<j & data_raw[[paste0("q",i,"_bin")]]==0, 1, 0)
+  }
+}
+
+## counts across cut-points ##
+for (i in 1:N){
+  # Total score
+  data_raw[[paste0("TP_t",i,sep="")]]<- ifelse(data_raw$Total_count>=i, data_raw$Total_count, NA)
+  data_raw[[paste0("FN_t",i,sep="")]]<- ifelse(data_raw$Total_count>=i, N-data_raw$Total_count, NA)
+  data_raw[[paste0("FP_t",i,sep="")]]<- ifelse(data_raw$Total_count<i, data_raw$Total_count, NA)
+  data_raw[[paste0("TN_t",i,sep="")]]<- ifelse(data_raw$Total_count<i, N-data_raw$Total_count, NA)
+}
+# If you don't have subscales, don't run line 71-82
+for (i in 1:N){
+  # scale 1
+  data_raw[[paste0("TP_s1",i,sep="")]]<- ifelse(data_raw$s1_count>=i, data_raw$s1_count, NA)
+  data_raw[[paste0("FN_s1",i,sep="")]]<- ifelse(data_raw$s1_count>=i, a-data_raw$s1_count, NA)
+  data_raw[[paste0("FP_s1",i,sep="")]]<- ifelse(data_raw$s1_count<i, data_raw$s1_count, NA)
+  data_raw[[paste0("TN_s1",i,sep="")]]<- ifelse(data_raw$s1_count<i, a-data_raw$s1_count, NA)
+  # scale 2
+  data_raw[[paste0("TP_s2",i,sep="")]]<- ifelse(data_raw$s2_count>=i, data_raw$s2_count, NA)
+  data_raw[[paste0("FN_s2",i,sep="")]]<- ifelse(data_raw$s2_count>=i, b-data_raw$s2_count, NA)
+  data_raw[[paste0("FP_s2",i,sep="")]]<- ifelse(data_raw$s2_count<i, data_raw$s2_count, NA)
+  data_raw[[paste0("TN_s2",i,sep="")]]<- ifelse(data_raw$s2_count<i, b-data_raw$s2_count, NA)
+}
+
+######### histogram #########
+par(mfrow=c(1,1))
+hist(data_raw$Total_count, xlab="Total count", main="Total histogram", breaks=as.integer(N), xlim=c(0,N))
+# If you don't have subscales, don't run line 88-89
+hist(data_raw$s1_count, xlab="s1 count", main="s1 histogram", breaks=as.integer(a), xlim=c(0,N))
+hist(data_raw$s2_count, xlab="s2 count", main="s2 histogram", breaks=as.integer(b), xlim=c(0,N))
+
+######### reshape table for calculating sensspec.. #########
+## sum over cutpoint ##
+varlist<- c("q_TP_5","q_TN_10","TP_t","FN_t","FP_t","TN_t")
+# If you don't have subscales, don't run line 95
+varlist<- c(varlist,"TP_s1","FN_s1","FP_s1","TN_s1","TP_s2","FN_s2","FP_s2","TN_s2")
+
+data_cutpoint<- data.frame(cutpoint)
+for (i in varlist){
+  i<- colSums(data_raw[grep(i, names(data_raw), value=TRUE)], na.rm=T, dims=1)
+  data_cutpoint<- data.frame(data_cutpoint, i) 
+}
+colnames(data_cutpoint)<- c("cutpoint", varlist)
+
+## define scales ##
+scales<- c("t")
+# If you don't have subscales, don't run line 107-109
+scales<- c(scales, "s1","s2")
+# generate subscale indicating subscale items
+data_cutpoint$subscale<- ifelse(data_cutpoint$cutpoint %in% as.numeric(substring(raw_items,2)),1,2)
+
+## Sensitivity and Specificity ##
+for (i in scales){
+  data_cutpoint[[paste0("sens_",i,sep="")]]<- 
+    data_cutpoint[[paste0("TP_",i,sep="")]]/
+    (data_cutpoint[[paste0("TP_",i,sep="")]]+data_cutpoint[[paste0("FN_",i,sep="")]])
+  data_cutpoint[[paste0("spec_",i,sep="")]]<- 
+    data_cutpoint[[paste0("TN_",i,sep="")]]/
+    (data_cutpoint[[paste0("TN_",i,sep="")]]+data_cutpoint[[paste0("FP_",i,sep="")]])
+  data_cutpoint[[paste0("spec_",i,"_1",sep="")]]<- 1-data_cutpoint[[paste0("spec_",i,sep="")]]
+}
+
+## Positive and Negative Predictive Power ##
+for (i in scales){
+  data_cutpoint[[paste0("PPP_",i,sep="")]]<- 
+    data_cutpoint[[paste0("TP_",i,sep="")]]/
+    (data_cutpoint[[paste0("TP_",i,sep="")]]+data_cutpoint[[paste0("FP_",i,sep="")]])
+  data_cutpoint[[paste0("NPP_",i,sep="")]]<- 
+    data_cutpoint[[paste0("TN_",i,sep="")]]/
+    (data_cutpoint[[paste0("TN_",i,sep="")]]+data_cutpoint[[paste0("FN_",i,sep="")]])
+}
+
+
+############
+# graphics #
+############
+
+## item plots ##
+# TP when positive responses are rare
+TP5<- data_raw[data_raw$Total_count<7,]
+TP5<- colSums(TP5[grep("q_TP_5", names(TP5), value=TRUE)], na.rm=T, dims=1)
+TP5<- data.frame(cutpoint, TP5) 
+ggplot(TP5,aes(x=cutpoint,y=TP5,fill=factor(cutpoint)))+
+  geom_bar(stat="identity",position="dodge")+
+  scale_x_continuous(breaks=1:12)+
+  scale_fill_discrete(name="item")+
+  theme(panel.background=element_blank())+
+  theme(panel.background= element_rect(color="black"))+
+  xlab("Item")+ylab("Count")
+
+TP3<- data_raw[data_raw$Total_count<7,]
+TP3<- colSums(TP3[grep("q_TP_3", names(TP3), value=TRUE)], na.rm=T, dims=1)
+TP3<- data.frame(cutpoint, TP3) 
+ggplot(TP3,aes(x=cutpoint,y=TP3,fill=factor(cutpoint)))+
+  geom_bar(stat="identity",position="dodge")+
+  scale_x_continuous(breaks=1:12)+
+  scale_fill_discrete(name="item")+
+  theme(panel.background=element_blank())+
+  theme(panel.background= element_rect(color="black"))+
+  xlab("Item")+ylab("Count")
+
+# TN when negative responses are rare
+TN10<- data_raw[data_raw$Total_count>7,]
+TN10<- colSums(TN10[grep("q_TN_10", names(TN10), value=TRUE)], na.rm=T, dims=1)
+TN10<- data.frame(cutpoint, TN10) 
+ggplot(TN10,aes(x=cutpoint,y=TN10,fill=factor(cutpoint)))+
+  geom_bar(stat="identity",position="dodge")+
+  scale_x_continuous(breaks=1:12)+
+  scale_fill_discrete(name="item")+
+  theme(panel.background=element_blank())+
+  theme(panel.background= element_rect(color="black"))+
+  xlab("Item")+ylab("Count")
+
+TN9<- data_raw[data_raw$Total_count>7,]
+TN9<- colSums(TN9[grep("q_TN_9", names(TN9), value=TRUE)], na.rm=T, dims=1)
+TN9<- data.frame(cutpoint, TN9) 
+ggplot(TN9,aes(x=cutpoint,y=TN9,fill=factor(cutpoint)))+
+  geom_bar(stat="identity",position="dodge")+
+  scale_x_continuous(breaks=1:12)+
+  scale_fill_discrete(name="item")+
+  theme(panel.background=element_blank())+
+  theme(panel.background= element_rect(color="black"))+
+  xlab("Item")+ylab("Count")
+
+# If you don't have subscale, don't run line 185-190
+## item by subscale ##
+ggplot(data_cutpoint, aes(x=cutpoint, y=q_TP_5, color=factor(subscale)))+
+  geom_point(aes(y = q_TP_5), size=5)+
+  theme(panel.background=element_blank())+
+  theme(panel.background= element_rect(color="black"))+
+  labs(x="Item", y="Count", color="Subscale")
+
+## sensitivity and specificity ##
+sensspec<- melt(data_cutpoint[,c("cutpoint","sens_t","spec_t")], id="cutpoint")
+ggplot(sensspec, aes(x=cutpoint, y=value, color=variable))+
+  geom_line(aes(y = value), size=1.2)+
+  theme(panel.background=element_blank())+
+  theme(panel.background= element_rect(color="black"))+
+  scale_x_continuous(breaks=1:12)+
+  coord_cartesian(ylim=c(0,1.1))+
+  theme(legend.key= element_blank(), legend.background= element_rect(color="black"))
+
+## PPP and NPP ##
+pppnpp<- melt(data_cutpoint[,c("cutpoint","PPP_t","NPP_t")], id="cutpoint")
+ggplot(pppnpp, aes(x=cutpoint, y=value, color=variable))+
+  geom_line(aes(y = value), size=1.2)+
+  theme(panel.background=element_blank())+
+  theme(panel.background= element_rect(color="black"))+
+  scale_x_continuous(breaks=1:12)+
+  coord_cartesian(ylim=c(0,1.1))+
+  theme(legend.key= element_blank(), legend.background= element_rect(color="black"))
+
+
